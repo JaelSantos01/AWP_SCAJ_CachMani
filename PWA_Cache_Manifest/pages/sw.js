b10d44abb62bcdf513a0fb3b5e7f4a0b375c3257
JaelSantos01 <20223tn110@utez.edu.mx>
2025-10-21 15:31:06 -0600

    Primera prueba del cache con manifest

---
+++ PWA_Cache_Manifest/pages/sw.js
@@ -0,0 +1,38 @@
+const cacheName = 'cache-offline-v1';
+const filesOffline = [
+    "/",
+    "/index.html",
+    "/pages/pagina2.html",
+    "/pages/error.html"
+]
+
+self.addEventListener('install', (event) =>{
+    event.waitUntil(
+        caches.open(cacheName)
+        .then((cache) => {
+            return cache.addAll(filesOffline);
+        })
+        .catch(err => console.log('Falló registro de caché', err))
+    );
+});
+
+self.addEventListener('fetch', event =>{
+    if(event.request.mode === 'navigate'){
+        event.respondWith(
+            fetch(event.request)
+            .then(response => {
+                return response;
+            })
+            .catch(() => {
+                return caches.open(cacheName)
+                .then(cache => {
+                    return cache.match(event.request)
+                    .then(response => {
+                        if(response) return response;
+                        return cache.match('/pages/error.html');
+                    })
+                })
+            })
+        )
+    }
+})
